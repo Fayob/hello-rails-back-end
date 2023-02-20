fcee1a697b75ccbb4d6f7df88013810481edec65
fayob <adedejifavour6@gmail.com>
2023-02-20 23:59:55 +0100

    Add rack cors gem

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,13 @@ module Backend
       end if File.exists?(env_file)
     end
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post]
+      end
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
